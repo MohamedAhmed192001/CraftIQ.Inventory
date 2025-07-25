bb71dfad21ecbc6821c7c9a8792188863e15aba8
MohamedAhmed192001 <dev.mohamed.ahmed.2001@gmail.com>
2025-07-25 17:10:35 +0300

    chore(database): configure DbContext with SQL Server and apply initial migration

---
+++ CraftlQ.Inventory.API/Program.cs
@@ -0,0 +1,29 @@
+using CraftIQ.Inventory.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var inventoryConnectionString = builder.Configuration.GetSection("ConnectionStrings:InventoryDbConnection");
+builder.Services.AddInventoryDbContext(inventoryConnectionString.Value!);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
